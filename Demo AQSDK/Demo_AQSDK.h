18598564cc3079261b7a91d0d5330c6a39c34734
Safiqul <speak2safi@gmail.com>
2020-01-05 00:51:29 +0530

    Initial Commit

---
+++ Demo AQSDK/Demo_AQSDK.h
@@ -0,0 +1,19 @@
+//
+//  Demo_AQSDK.h
+//  Demo AQSDK
+//
+//  Created by SAFIQUL ISLAM on 05/01/20.
+//  Copyright © 2020 Acquire-sdk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Demo_AQSDK.
+FOUNDATION_EXPORT double Demo_AQSDKVersionNumber;
+
+//! Project version string for Demo_AQSDK.
+FOUNDATION_EXPORT const unsigned char Demo_AQSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Demo_AQSDK/PublicHeader.h>
+
+
